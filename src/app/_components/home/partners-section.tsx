eea7db4590e01b2ea1029eab3400d4d4339acb63
devbn3li <muhammed.ali.hissein@gmail.com>
2025-01-20 18:34:45 +0200

    partners section

--- src/app/_components/home/partners-section.tsx
+++ src/app/_components/home/partners-section.tsx
@@ -1,70 +1,47 @@
-import Image from "next/image";
-import { Title } from "../typography";
-import { Sparkles } from "../ui";
+import Image from 'next/image';
+import { cn } from '~/lib/utils';
 
-export default function () {
-  const title = "Trusted By";
-  const partners = [
-    {
-      id: 1,
-      imageSrc: "/images/partenars/actus-go.svg",
-    },
-    {
-      id: 2,
-      imageSrc: "/images/partenars/levelup.png",
-      name: "Level Up ESG",
-    },
-    {
-      id: 3,
-      imageSrc: "/images/partenars/neferdata.png",
-      name: "Neferdata",
-    },
-    {
-      id: 4,
-      imageSrc: "/images/partenars/talabatak.webp",
-      name: "Talabatak",
-    },
-  ];
+const companies = [
+  { name: 'actusgo', logo: '/images/partenars/actusgo.png' },
+  { name: 'digital', logo: '/images/partenars/DigitalOcean_logo.png' },
+  { name: 'levelup', logo: '/images/partenars/levelup.png' },
+  { name: 'Netflix', logo: '/images/partenars/actusgo.png' },
+  { name: 'YouTube', logo: '/images/partenars/actusgo.png' },
+  { name: 'Instagram', logo: '/images/partenars/actusgo.png' },
+];
 
+export default function PartnersSection(): JSX.Element {
   return (
-    <div className="w-full">
-      <div className="mx-auto w-full max-w-7xl p-20 px-2">
-        <Title>{title}</Title>
-
-        <div className="relative flex min-h-20 w-full items-center justify-center gap-12">
-          {partners.map((partner) => (
-            <div
-              key={partner.id}
-              className="flex flex-col items-center justify-center gap-2"
-            >
-              <div className="relative z-10 flex h-20 w-64 items-center justify-center overflow-hidden px-4">
-                <Image
-                  src={partner.imageSrc}
-                  alt="id"
-                  fill
-                  className="max-h-full max-w-full object-contain"
-                />
-              </div>
-
-              {partner.name && (
-                <span className="mt-2 text-center font-semibold">
-                  {partner.name}
-                </span>
-              )}
+    <section id="companies">
+      <div className="py-14">
+        <div className="container mx-auto px-4 md:px-8">
+          <h3
+            className={cn(
+              `flex -translate-y-4 flex-col items-center justify-center text-balance bg-gradient-to-br from-white from-30% to-primary-100/40 bg-clip-text py-6 text-center text-4xl font-medium leading-none tracking-tighter text-transparent md:text-4xl lg:text-6xl 2xl:text-7xl`,
+            )}
+          >
+            TRUSTED BY LEADING TEAMS
+          </h3>
+          <div className="relative mt-6">
+            <div className="grid grid-cols-2 place-items-center gap-4 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8">
+              {companies.map((company, idx) => (
+                <div
+                  key={idx}
+                  className="flex h-20 w-full items-center justify-center p-2" // Centering container
+                >
+                  <Image
+                    src={company.logo}
+                    width={160}
+                    height={40}
+                    className="h-auto w-full max-w-[120px] object-contain filter grayscale transition-all hover:filter-none" // Responsive and centered
+                    alt={company.name}
+                  />
+                </div>
+              ))}
             </div>
-          ))}
-          <div className="absolute inset-x-0 top-[25%] z-0 h-80 w-full overflow-hidden [mask-image:radial-gradient(50%_50%,theme(colors.primary.100),transparent)] before:absolute before:inset-0 before:bg-[radial-gradient(circle_at_bottom_center,theme(colors.primary.150/80%),transparent_90%)] before:opacity-100 after:absolute after:-left-1/2 after:top-1/2 after:aspect-[1/1.8] after:w-[200%] after:rounded-[50%] after:border-2 after:border-b after:border-primary-800 after:bg-primary-900">
-            <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,theme(colors.primary.100/50%)_1px,transparent_1px)] bg-[size:70px_80px]"></div>
-            <Sparkles
-              density={1200}
-              size={1.4}
-              hover
-              direction="top"
-              className="absolute inset-x-0 top-0 h-full w-full [mask-image:radial-gradient(50%_50%,white,transparent_85%)]"
-            />
           </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 }
